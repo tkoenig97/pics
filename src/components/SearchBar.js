a1df37e007f0d70da487ac95854fc4dd84076f9e
tkoenig97 <tylerskoenig@gmail.com>
2022-10-29 14:27:30 -0500

    Added onFormSubmit function

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,16 +3,20 @@ import React from 'react';
 class SearchBar extends React.Component {
     state = { term: '' };
 
+    onFormSubmit(event) {
+        event.preventDefault();
+    }
+
     render() {
         return (
             <div className='ui segment'>
-                <form className='ui form'>
+                <form onSubmit={this.onFormSubmit}className='ui form'>
                     <div className='field'>
                         <label>Image Search</label>
                         <input 
                             type='text' 
                             value={this.state.term}
-                            onChange={(e) => this.setState({ term: e.target.value })} 
+                            onChange={e => this.setState({ term: e.target.value })} 
                         />
                     </div>
                 </form>
